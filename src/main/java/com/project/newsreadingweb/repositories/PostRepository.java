6686d1000f8d306f5d3c1f02831cd672fdf45531
plz972k1 <84142100+plz972k1@users.noreply.github.com>
2023-07-02 20:24:49 +0700

    Search API

--- src/main/java/com/project/newsreadingweb/repositories/PostRepository.java
+++ src/main/java/com/project/newsreadingweb/repositories/PostRepository.java
@@ -5,9 +5,10 @@ import com.project.newsreadingweb.entities.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
 import java.util.UUID;
 
 @RepositoryRestResource(collectionResourceRel = "posts", path = "posts", excerptProjection = PostWithAuthorProjection.class)
 public interface PostRepository extends JpaRepository<Post, UUID> {
-
+    List<Post> findByTitleContainingOrBodyContaining(String titleKeyword, String bodyKeyword);
 }
